b8e824f1c317c823ce469dd7baaea8b573a8a2ab
Bilal <nassiribilal2006@gmail.com>
2025-07-02 18:00:20 +0100

    Implement Sign in AND change database password length from varchar(40) to varchar(255) fixes login problem

---
+++ signin.php
@@ -0,0 +1,65 @@
+<?php
+require_once 'Connect.php';
+function dd($var)
+{
+    echo "<pre>";
+
+    var_dump($var);
+    echo "</pre>";
+    die();
+}
+try{
+    $sql = new Connect();
+    $db = $sql->conn;
+}catch(mysqli_sql_exception $e){
+    echo "Can't connect to the database .";
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $email = trim($_POST["email"]);
+    $password = trim($_POST["password"]);
+//$2y$10$RWTm5lUFPW7VxiIWKjAvoexlwpNzCrXaK
+    function emailValidation($email){
+        // EMAIL VALIDAION
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            header("Location: signup_signin.view.php?email_error=Invalid Email !");
+            return false;
+        }else if(empty($email)){
+            header("Location: signup_signin.view.php?email_error=Please enter an email !");
+            return false;
+        }
+        else{
+          return true;
+        }
+
+    }
+    function passValidation($password){
+        if(empty($password)){
+            header("Location: signup_signin.view.php?password_error=Please enter a password !");
+            return false;
+        }else{
+            return true;
+        }
+    }
+    function signin($email, $password,$db){
+        $search_query = "SELECT email, password FROM registration WHERE email='$email'";
+
+        $result = mysqli_query($db, $search_query);
+        $user = mysqli_fetch_assoc($result);
+        if(mysqli_num_rows($result) === 1){
+            if(password_verify($password, $user['password'])) {
+                header("Location: user.php");
+                exit();
+            }else{
+                header("Location: signup_signin.view.php?password_error=Your password is incorrect !");
+                exit();
+            }
+        }else{
+            header("Location: signup_signin.view.php?signin_error=User doesn't exist. Register an account !");
+            exit();
+        }
+    }
+    if(emailValidation($email) && passValidation($password)){
+        signin($email, $password, $db);
+    }
+}
--- signup_signin.view.php
+++ signup_signin.view.php
@@ -14,27 +14,36 @@
         <img src="Images/img.png" alt="leaves">
         <h2 class="text-right">Welcome</h2>
         <div class="form-wrapper login">
-            <form action="signup.php" method="post">
+            <form action="signin.php" method="post">
                 <h2>Login</h2>
                 <div class="input-box">
                     <span class="icon">
                         <ion-icon name="mail"></ion-icon>
                     </span>
                     <input type="email" placeholder="Email"  name="email" required>
+                    <p style="color:darkred;margin-top:10px;"><?php echo isset($_GET['email_error']) ? $_GET['email_error'] : '';?></p>
                 </div>
                 <div class="input-box">
                     <span class="icon">
                         <ion-icon name="lock-closed"></ion-icon>
                     </span>
                     <input type="password" placeholder="Password"   name="password" required>
+                    <p style="color:darkred;margin-top:10px;"><?php echo isset($_GET['password_error']) ? $_GET['password_error'] : '';?></p>
+
                 </div>
-                <div class="forgot-pass">
-                    <a href="#">Forgot Password?</a>
-                </div>
+<!--                <div class="forgot-pass">-->
+<!---->
+<!--                    <a href="#">Forgot Password?</a>-->
+<!--                </div>-->
+
                 <button type="submit">Login</button>
+                <p style="color:darkred;margin-top:10px;"><?php echo isset($_GET['signin_error']) ? $_GET['signin_error'] : '';?></p>
+
+
                 <div class="sign-link">
                     <p>Don't have an account? <a href="#" onclick="registerActive()">Register</a></p>
                 </div>
+
             </form>
         </div>
         <div class="form-wrapper register">
@@ -68,5 +77,6 @@
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <script src="signup.js"></script>
+
 </body>
 </html>
